7ef6094159b892ea3967fbecf7815beb2743a249
Guillermo-Galdamez <00084320@uca.edu.sv>
2020-11-19 03:52:45 -0600

    Avance nuevo

--- proyect_avance7.cpp
+++ proyect_avance7.cpp
@@ -33,18 +33,7 @@ struct Desaparecido
     string apellido;
     int edad;
 }; 
-void ingresar(struct Afectado afectado){
-  bool cont=true;
-  
-  do{
-           cout << "Ingrese primer y segundo nombre \n";
-           getline(cin, afectado.nombre); cout << endl;
-           cout << "Ingrese primer y segundo apellido \n";
-           getline(cin, afectado.apellido); cout << endl;
-           cout << "Ingrese edad \n";
-           cin >> afectado.edad;
-  }while(cont);
-}
+
 void damnificados(list <Afectado> Damnificados){
     bool m = true, continuar = true, continuar2=true;
     int option, n, o;
@@ -72,32 +61,32 @@ void damnificados(list <Afectado> Damnificados){
            cout << "Ingrese primer y segundo apellido \n";
            getline(cin, afectado.apellido); cout << endl;
            cout << "Ingrese edad \n";
-           cin >> afectado.edad;
-           
+           cin>>afectado.edad;
+        
            printf("Ingrese necesidad que presenta: \n 1. Ropa\n 2.Cama\n 3.Medicina\n 4. Art%cculos de limpieza personal\n",161);
            while (continuar)
             {cin >> n;
            switch(n){
                case 1 : nec.insert(nec.end(),Ropa);
-               cout << "Desea continuar? (y/n)";
+               cout << "Existe otra necesidad? (y/n)";
                cin >> c;
                if (c == 'n') continuar = false;
                break;
 
                case 2 : nec.insert(nec.end(),Cama); 
-               cout << "Desea continuar? (y/n)";
+               cout << "Existe otra necesidad? (y/n)";
                cin >> c;
                if (c == 'n') continuar = false;
                break;
 
                case 3 : nec.insert(nec.end(),Medicina);
-               cout << "Desea continuar? (y/n)";
+               cout << "Existe otra necesidad? (y/n)";
                cin >> c;
                if (c == 'n') continuar = false;
                break;
 
                case 4 : nec.insert(nec.end(),LimpiezaPersonal);
-               cout << "Desea continuar? (y/n)";
+               cout << "Existe otra necesidad? (y/n)";
                cin >> c;
                if (c == 'n') continuar = false;
                break;
@@ -174,13 +163,13 @@ void damnificados(list <Afectado> Damnificados){
                 cout <<" Albergue: ";
                 switch(i.alb){
                   case NJ01: cout << "Centro Escolar Aldea de las Mercedes (Nejapa)" << endl; break;
-                  case NJ02: cout << "Iglesia Evang(e)lica Siloe (Nejapa)" << endl; break;
-                  case NJ03: cout << "Centro Escolar Jos(e) Matias Delgado (San salvador)" << endl; break;
-                  case SS01: cout << "Cancha de F(u)tbol Chapupo Rodr(i)guez (San salvador)" << endl; break;
+                  case NJ02: printf("Iglesia Evang%clica Silo%c (Nejapa)\n",130,130); break;
+                  case NJ03: printf("Centro Escolar Jos%c Mat%cas Delgado (San salvador)\n",130,161); break;
+                  case SS01: printf("\n4. Cancha de F%ctbol Chapupo Rodr%cguez (San salvador) \n",163,161); break;
                   case SS02: cout << "Parque Dr. Carlos Antonio Herrera Rebollo (San salvador)" << endl; break;
-                  case SS03: cout << "Iglesia Catolica Don Rua (San salvador)" << endl; break;
-                  case AC01: cout << "Centro educativo Walter Thilo Deininger (Antiguo Cuscatlan)" << endl; break;
-                  case AC02: cout << "Complejo Deportivo La sultana (Antiguo Cuscatlan)" << endl; break;
+                  case SS03: printf("Iglesia Cat%clica Don Rua (San salvador) \n",162); break;
+                  case AC01: printf("Centro educativo Walter Thilo Deininger (Antiguo Cuscatl%cn)\n",160);; break;
+                  case AC02: printf("Complejo Deportivo La sultana (Antiguo Cuscatl%cn)\n",160); break;
                 }
            }
            cout<<endl;
@@ -214,18 +203,20 @@ void heridos( list <Herido> Heridos){
         printf("*Introduzca el n%cmero de la ",163);
         printf("acci%cn a realizar*\n",162);
 
-        cin >> option;cout<<endl;
+        cin >> option;cout<<endl;cin.ignore();
         switch (option)
         {
         case 1:
-            cout << "Ingrese primer y segundo nombre \n";
+           cout << "Ingrese primer y segundo nombre \n";
            getline(cin, herido.nombre); cout << endl;
            cout << "Ingrese primer y segundo apellido \n";
            getline(cin, herido.apellido); cout << endl;
            cout << "Ingrese edad \n";
            cin >> herido.edad;
-
-           cout << "Ingrese Problema que presenta\n 1. Hueso_roto 2.Laceraciones 3.Enfermedad_respiratoria 4.Herida_craneal 5.Enfermedad_grave \n";
+           cin.ignore();
+           printf("Ingrese al hospital al que se le llevar%c\n",160);
+            getline(cin, herido.hospital); cout << endl;
+           cout << "Ingrese Problema que presenta\n 1. Hueso_roto\n 2.Laceraciones\n 3.Enfermedad_respiratoria\n 4.Herida_craneal\n 5.Enfermedad_grave \n";
            cin>>n;cout<<endl;
            switch(n){
                case 1 : herido.in = Hueso_roto; continuar = false; 
@@ -242,8 +233,7 @@ void heridos( list <Herido> Heridos){
                     printf("v%clida\n",160);
                break;
            }
-           printf("Ingrese al hospital al que se le llevar%c\n",160);
-            getline(cin, herido.hospital); cout << endl;
+           
             Heridos.insert(Heridos.end(), herido);
 
             break;
@@ -312,7 +302,7 @@ void desaparecidos(list <Desaparecido> Desaparecidos){
         cout << "4. Volver" << endl;
         printf("*Introduzca el n%cmero de la ",163);
         printf("acci%cn a realizar*\n",162);
-        cin >> option;cout<<endl;
+        cin >> option;cout<<endl;cin.ignore();
         switch (option)
         {
         case 1:
